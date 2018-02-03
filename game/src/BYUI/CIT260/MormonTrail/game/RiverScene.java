2917af280a3d18d1a333baf33c5f68f15a934aae
Chaskiel <35944396+Chaskiel@users.noreply.github.com>
2018-02-02 17:08:19 -0700

    Code

--- game/src/BYUI/CIT260/MormonTrail/game/RiverScene.java
+++ game/src/BYUI/CIT260/MormonTrail/game/RiverScene.java
@@ -3,12 +3,76 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package BYUI.CIT260.MormonTrail.game;
+
+package byui.cit260.mormontrail.model;
+import java.io.Serializable;
+import java.util.Objects;
+
 
 /**
  *
- * @author Chas
+ * @author Chaskiel
  */
-public class RiverScene {
+public class River implements Serializable{
     
+    
+     private String weight;
+     
+     private String riverDepth; 
+     
+     public River(){ 
+}
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getRiverDepth() {
+        return riverDepth;
+    }
+
+    public void setRiverDepth(String riverDepth) {
+        this.riverDepth = riverDepth;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Objects.hashCode(this.weight);
+        hash = 43 * hash + Objects.hashCode(this.riverDepth);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "River{" + "weight=" + weight + ", riverDepth=" + riverDepth + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final River other = (River) obj;
+        if (!Objects.equals(this.weight, other.weight)) {
+            return false;
+        }
+        if (!Objects.equals(this.riverDepth, other.riverDepth)) {
+            return false;
+        }
+        return true;
+    }
+     
 }
