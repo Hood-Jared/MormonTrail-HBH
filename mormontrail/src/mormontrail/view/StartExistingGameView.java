3f49a5e506343c63221b11b8a360a8f1c0c86824
jared <jared@DESKTOP-QR2Q15T.attlocal.net>
2018-02-24 20:12:29 -0500

    Merge origin/master
    Conflicts:
    	mormontrail/src/mormontrail/view/MainMenuView.java

---
+++ mormontrail/src/mormontrail/view/StartExistingGameView.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mormontrail.view;
+
+import mormontrail.model.Player;
+
+/**
+ *
+ * @author jared
+ */
+public class StartExistingGameView {
+    
+    public static void displayStartExistingGameView() {
+		System.out.println("*** Start Existing Game()called ***");
+    
+}
+
+    void display() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
