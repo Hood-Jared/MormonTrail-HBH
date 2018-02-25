709262d8762c9464df09d3d7bd0a254e65b510fe
Heather Holt <hol16046@byui.edu>
2018-02-24 18:40:55 -0700

    doAction

--- mormontrail/src/mormontrail/controller/GameController.java
+++ mormontrail/src/mormontrail/controller/GameController.java
@@ -60,6 +60,22 @@ END */
   }
 
 	public static Player savePlayer(String playersName) {
+			/* savePlayer(name): Player
+			BEGIN
+			if name is null OR length of name is < 1 THEN
+			RETURN null
+			ENDIF
+			player = new Player object
+			save the name in the player object
+			save the player in the main class of the project
+			RETURN player
+			END */
+		int length = playersName.length();
+		if (playersName == null || length < 1) {
+			return null;
+		}	
+		Player player = new Player();
+		
 		System.out.println("*** savePlayer()called ***");
 		return new Player();
 	}
