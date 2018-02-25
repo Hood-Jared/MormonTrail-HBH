e02aaad7786c00e6c53f38152d06f86a6c90a694
jared <jared@DESKTOP-QR2Q15T.attlocal.net>
2018-02-24 20:21:25 -0500

    Merge origin/master
    Conflicts:
    	mormontrail/src/mormontrail/view/MainMenuView.java

--- mormontrail/src/mormontrail/view/StartExistingGameView.java
+++ mormontrail/src/mormontrail/view/StartExistingGameView.java
@@ -21,3 +21,5 @@ public class StartExistingGameView {
     void display() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+}
