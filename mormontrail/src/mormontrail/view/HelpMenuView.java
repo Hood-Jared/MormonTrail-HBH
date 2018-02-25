42cd0895cb9e78226f5e59ff5f0bfda11631507e
Heather Holt <hol16046@byui.edu>
2018-02-24 20:30:21 -0700

    HelpMenuView

---
+++ mormontrail/src/mormontrail/view/HelpMenuView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mormontrail.view;
+
+/**
+ *
+ * @author heatherholt
+ */
+public class HelpMenuView {
+
+	void display() {
+		System.out.println("*** displayHelpMenuView called ***");
+	}
+	
+}
