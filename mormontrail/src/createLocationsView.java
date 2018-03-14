37e596dcaa67d2ee262de7a6a148812372a8323c
Chaskiel <35944396+Chaskiel@users.noreply.github.com>
2018-03-14 13:24:14 -0600

    doAction

---
+++ mormontrail/src/createLocationsView.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mormontrail.controller;
+import java.io.Serializable;
+import java.util.Objects;
+import mormontrail.Mormontrail;
+import mormontrail.model.Location;
+import mormontrail.model.Player;
+/**
+ *
+ * @author Chaskiel
+ */
+public class createLocationsView {
+    
+    public static Location[][] createLocations (int rows, int columns){
+		
+			
+	if (rows <1 || columns < 1){
+           return null;
+        }
+            //ENDIF? 
+	int locations [][] = new int Location [][];
+             
+		
+	
+    }
+}
---
+++ mormontrail/src/mormontrail/view/GameMenuView.java
@@ -0,0 +1,17 @@
+package mormontrail.view;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Chaskiel
+ */
+public class GameMenuView {
+    
+}
+
+
